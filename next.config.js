5a35ee3907db58c11722e2f3feafe082856b92af
MaoMaoPrince <31109364+MaoMaoPrince@users.noreply.github.com>
2025-04-17 16:28:56 +0100

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     remotePatterns: [],
     unoptimized: true
   },
+  basePath: '/vehicle_vault',
 }
 
 module.exports = nextConfig 
